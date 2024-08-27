3b70a8cde1381926082a87b112397605e9df423d
Kartik Mishra <kartikmishra.km44@gmail.com>
2024-08-27 21:00:00 +0530

    stats and services

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,5 +1,81 @@
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { easeIn, motion } from "framer-motion";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe obcaecati, incidunt!",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "UI/UX Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe obcaecati, incidunt!",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "Logo Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe obcaecati, incidunt!",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe obcaecati, incidunt!",
+    href: "",
+  },
+];
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: easeIn },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[40px]"
+        >
+          {services.map((service, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col justify-center flex-1 gap-6 group"
+              >
+                <div className="flex items-center justify-between w-full">
+                  <div className="text-5xl font-extrabold text-transparent transition-all duration-500 text-outline group-hover:text-outline-hover">
+                    {service.num}
+                  </div>
+                  <Link
+                    href={service.href}
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-3xl text-primary" />
+                  </Link>
+                </div>
+                <div className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {service.title}
+                </div>
+                <p className="text-white/60">{service.description}</p>
+                <div className="w-full border-b border-white/20"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,54 @@
 "use client";
 
+import CountUp from "react-countup";
+
+const stats = [
+  {
+    num: "8",
+    text: "Years of experience",
+  },
+  {
+    num: "30",
+    text: "Projects completed",
+  },
+  {
+    num: "40",
+    text: "Technologies mastered",
+  },
+  {
+    num: "200",
+    text: "Code commits",
+  },
+];
+
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+          {stats.map((stat, index) => {
+            return (
+              <div className="flex items-center justify-center flex-1 gap-4 xl:justify-start">
+                <CountUp
+                  end={stat.num}
+                  duration={5}
+                  delay={2}
+                  className="text-4xl font-extrabold xl:text-6xl"
+                />
+                <p
+                  className={`${
+                    stat.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  } leading-snug text-white/80`}
+                >
+                  {stat.text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Stats;
