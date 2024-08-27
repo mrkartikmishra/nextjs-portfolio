116796a5a78f1bbd39e954ca096f1fe1408e2215
Kartik Mishra <kartikmishra.km44@gmail.com>
2024-08-27 23:12:10 +0530

    content update

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -28,7 +28,7 @@ const services = [
   },
   {
     num: "04",
-    title: "Full Stack Development",
+    title: "Fullstack Development",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe obcaecati, incidunt!",
     href: "",
