8716dc8bd4f80e77142d49efd83837d9a4f69277
Kartik Mishra <kartikmishra.km44@gmail.com>
2024-08-30 21:52:46 +0530

    contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,115 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const contactInfo = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+91) 9732107674",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "kartikmishra.km44@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Current location",
+    description: "Hyderabad, INDIA",
+  },
+];
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:w-[54%] order-2 xl:order-none">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+              <h3 className="text-4xl text-accent">Let's work together</h3>
+              <p className="text-white/60">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint
+                aut totam alias sequi, impedit quos perspiciatis explicabo
+                repellat fugit omnis.
+              </p>
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                <Input type="firstname" placeholder="First name" />
+                <Input type="lastname" placeholder="Last name" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone number" />
+              </div>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="blockchain">
+                      Blockchain development
+                    </SelectItem>
+                    <SelectItem value="fullstack">
+                      Full stack development
+                    </SelectItem>
+                    <SelectItem value="backend">Backend development</SelectItem>
+                    <SelectItem value="frontend">
+                      Frontend development
+                    </SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your message here..."
+              />
+              <Button size="md" className="max-w-40">
+                Send message
+              </Button>
+            </form>
+          </div>
+          <div className="flex items-center flex-1 order-1 mb-8 xl:justify-end xl:order-none xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {contactInfo.map((info, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] xl:w-[72px] bg-[#2727c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{info.icon}</div>
+                    </div>
+                    <div className="felx-1">
+                      <p className="text-white/60">{info.title}</p>
+                      <h3 className="text-xl">{info.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 export default Contact;
--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,3 +1,22 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import { motion } from "framer-motion";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const projects = [{}];
+
 const Work = () => {
   return <div>Work</div>;
 };
